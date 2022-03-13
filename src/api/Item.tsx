46f20030b8c2ce6ab5892c6930d70a199ff16110
patrick-petersen <patrick-petersen@users.noreply.github.com>
2022-03-13 19:07:47 +0100

    Added fish data as json

--- src/api/Item.tsx
+++ src/api/Item.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {ItemJson, state} from "./GW2Api";
+import {FishData} from "./FishData";
 
 class Item extends React.Component<ItemJson, ItemJson> {
 
@@ -8,7 +9,7 @@ class Item extends React.Component<ItemJson, ItemJson> {
     }
 
     render() {
-        return <p><img src={this.props.icon}/>{this.props.name}</p>
+        return <p><img src={this.props.icon}/>{this.props.name}<p>{FishData.getFishByName(this.props.name)?.toString()}</p></p>
     }
 
 }
