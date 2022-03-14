4d99527317bfa9b05ccd638e44de3be7521bd699
patrick-petersen <patrick-petersen@users.noreply.github.com>
2022-03-14 18:13:53 +0100

    Added Icon alt text

--- src/UI/Item.tsx
+++ src/UI/Item.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import {ItemJson, state} from "../api/GW2Api";
+import {ItemJson} from "../api/GW2Api";
 import {FishData} from "../api/FishData";
 
 type ItemProps = ItemJson & {progress: boolean};
 
 class Item extends React.Component<ItemProps, ItemJson> {
-
-    constructor(props: ItemProps) {
-        super(props);
-    }
-
     render() {
         const metadata = FishData.getFishByName(this.props.name);
         return <div className={"fish" + (this.props.progress?" done":" todo")}>
-            <img src={this.props.icon}/>
+            <img alt={this.props.name + " icon"} src={this.props.icon}/>
             <span>{this.props.name}</span>
             <span>{metadata?.Bait}</span>
             <span>{metadata?.["Open Water"]?"Open Water":""}</span>
