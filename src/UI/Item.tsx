4dba7c00db99b665a50f80d899f27cab5e78a5e9
patrick-petersen <patrick-petersen@users.noreply.github.com>
2022-05-21 15:09:52 +0200

    Added rarity for fish names

--- src/UI/Item.tsx
+++ src/UI/Item.tsx
@@ -7,9 +7,9 @@ type ItemProps = ItemJson & {progress: boolean};
 class Item extends React.Component<ItemProps, ItemJson> {
     render() {
         const metadata = FishData.getFishByName(this.props.name);
-        return <div className={"fish" + (this.props.progress?" done":" todo")}>
+        return <div className={"fish" + (this.props.progress?" done":" todo") + (" rarity-" + this.props.rarity)}>
             <img alt={this.props.name + " icon"} src={this.props.icon}/>
-            <span>{this.props.name}</span>
+            <span className={"name"}>{this.props.name}</span>
             <span>{metadata?.Bait}</span>
             <span>{metadata?.["Open Water"]?"Open Water":""}</span>
         </div>
