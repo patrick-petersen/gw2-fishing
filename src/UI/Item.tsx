ad80b2ecc5acf1a6d431630e4ff13af4fe879bad
patrick-petersen <patrick-petersen@users.noreply.github.com>
2022-06-02 20:41:14 +0200

    Added wiki link for fish

--- src/UI/Item.tsx
+++ src/UI/Item.tsx
@@ -9,7 +9,7 @@ class Item extends React.Component<ItemProps, ItemJson> {
         const metadata = FishData.getFishByName(this.props.name);
         return <div className={"fish" + (this.props.progress?" done":" todo") + (" rarity-" + this.props.rarity)}>
             <img alt={this.props.name + " icon"} src={this.props.icon}/>
-            <span className={"name"}>{this.props.name}</span>
+            <span className={"name"}><a href={"https://wiki.guildwars2.com/wiki/" + this.props.name} target={"_blank"} rel={"noreferrer"}>{this.props.name}</a></span>
             <span>{metadata?.Bait}</span>
             <span>{metadata?.["Open Water"]?"Open Water":""}</span>
         </div>
